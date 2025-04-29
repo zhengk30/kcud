fbe9d721298c2d26e74a72aa04b90aee7914ce97
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2025-04-29 15:39:59 +0800

    added newline character as separator between strings

--- src/regduck_api.cpp
+++ src/regduck_api.cpp
@@ -16,20 +16,19 @@ size_t load_strings_in_batch(char* batch_buf, size_t batch_size) {
     if (table == nullptr) {
         _table_init();
     }
-    batch_size = MIN(batch_size, MAX_BATCH_SIZE);
-    size_t current_size = 0;
+    // batch_size = MIN(batch_size, MAX_BATCH_SIZE);
     size_t actual_size = 0;
     size_t i = 0;
     char* str = nullptr;
     while ((str = table->GetNextString())) {
         size_t len = strlen(str);
-        current_size += len;
-        if (current_size > batch_size) {
-            actual_size = current_size - len;
+        if (actual_size + len + 1 > batch_size) {
             break;
         }
         memcpy(batch_buf + i, str, len);
-        i += len;
+        batch_buf[i + len] = '\n';
+        i += (len + 1);
+        actual_size += (len + 1);
         table->AdvanceCursors();
     }
     return actual_size;
